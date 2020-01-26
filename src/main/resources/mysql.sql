8b948d3ba50c443d203a7e246418a5766c208828
rohid dev <rohiddev@rohids-MacBook-Air.local>
2020-01-26 09:41:23 -0700

    u

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -11,9 +11,9 @@ INSERT INTO cities(name, population) VALUES('New York', 8550000);
 INSERT INTO cities(name, population) VALUES('Edinburgh', 464000);
 INSERT INTO cities(name, population) VALUES('Berlin', 3671000);
 
-CREATE TABLE `expense` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `item` varchar(45) NOT NULL,
-  `amount` float NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE expense (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  item varchar(45) NOT NULL,
+  amount float NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
